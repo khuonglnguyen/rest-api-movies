275b59c0730d2169092b33b10e7f5af63bb8a7fc
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-07-25 13:35:17 +0700

    Arrow function

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let express=require('express');
 let app=express();
 
-app.get('/',function (req,res){
+let indexHandler=(req,res) => {
     return res.send('hello world');
-});
+};
 
-app.get('/bye',function (req,res){
+app.get('/', indexHandler);
+
+app.get('/bye', (req,res) =>{
     return res.send('see you again');
 });
 
-app.listen(8000,function (){
+app.listen(8000, () =>{
    console.log('server started at: 127.0.0.1:8000')
 });
